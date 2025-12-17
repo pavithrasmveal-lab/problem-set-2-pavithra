224631ade6e7dbad1d9ec758f89b70db93fb6871
pavithrasmveal-lab <pavithrasmveal@gmail.com>
2025-12-17 21:26:49 +0530

    Update assign1.py

--- assign1.py
+++ assign1.py
@@ -2,7 +2,9 @@ list1=[]    #create first list
 list2=[]    #crate second list
 s1=int(input("enter size of list1:"))#get size of list1
 #to read elements in list1
-list1=list(map(int,input("Enter elements:").split()))
+for i in range(s1):
+    a=int(input("Enter element:"))
+    list1.append(a)
 #it will work for all except last element
 for i in range(0,s1-1):     
     list2.append(list1[i]+list1[i+1])
@@ -11,4 +13,5 @@ list2.append(list1[s1-1])
 #display of output
 print("The original list:",list1)   
 print("The changed one:",list2)
+
 
