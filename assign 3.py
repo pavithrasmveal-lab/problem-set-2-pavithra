da17bbad6b25d983003b51728c027228acce29ca
pavithrasmveal-lab <pavithrasmveal@gmail.com>
2025-12-27 17:26:24 +0530

    Update assign 3.py

--- assign 3.py
+++ assign 3.py
@@ -5,9 +5,10 @@ count=0      #to keep the output count
 for i in range((len(l)-sub)+1):
     sum1=0      #temporary variable to sum and avergae
     avg1=0
-    for j in range(i,i+sub):   #to run in lent=gth of sublist
+    for j in range(i,i+sub):   #to run in length of sublist
         sum1=sum1+int(l[j])
         avg1=sum1/sub
     if(avg1>=avg):    #check for average and increament count
         count+=1
 print(f"The count is:{count}")
+
