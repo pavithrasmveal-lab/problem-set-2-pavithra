3f483f139d51d08f1bcd2b74a04b280c849ebecf
pavithrasmveal-lab <pavithrasmveal@gmail.com>
2026-01-01 16:53:26 +0530

    Update assign1.py

--- assign1.py
+++ assign1.py
@@ -1,17 +1,11 @@
-list1=[]    #create first list 
-list2=[]    #crate second list
-s1=int(input("enter size of list1:"))#get size of list1
-#to read elements in list1
-for i in range(s1):
-    a=int(input("Enter element:"))
-    list1.append(a)
-#it will work for all except last element
-for i in range(0,s1-1):     
-    list2.append(list1[i]+list1[i+1])
-#append the last element
-list2.append(list1[s1-1])
-#display of output
-print("The original list:",list1)   
+#new list generation given a list(m) and output(n) a new list of same length but i-th element is defined as n[i]=m[i]+m[i+1] except last element
+def new_list(list1):
+    list2=[]     #output list
+    for i in range(0,len(list1)-1):    #it will work for all except last element 
+        list2.append(list1[i]+list1[i+1])
+    list2.append(list1[len(list1)-1])  #append the last element
+    return list2          #return output
+list1=list(map(int,input("Enter inputs:").split())) #input list
+list2=new_list(list1)      #function call
+print("The original list:",list1)    #display output
 print("The changed one:",list2)
-
-
