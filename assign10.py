86ee9f0dbe1c1d755b72df5b6628ea64bd9ab874
pavithrasmveal-lab <pavithrasmveal@gmail.com>
2026-01-03 06:53:46 +0530

    Update assign10.py

--- assign10.py
+++ assign10.py
@@ -1,24 +1,26 @@
-
-file=open("sensor_data.txt","r")
-distance=[]
+#Robot sensor program
+file=open("sensor_data.txt","r") #open file in read mode
+distance=[]    #list formation
 for line in file:
     line=line.strip()
-    if line.startswith('#') or line=="":
+    if line.startswith('#') or line=="":  #to ignore line start with #
         continue
-    distance.append(float(line))
-total_readings=len(distance)
-maximum_reading=max(distance)
-minimum_reading=min(distance)
+    distance.append(float(line))   #add remaing readings
+total_readings=len(distance)       # for total_readingd
+maximum_reading=max(distance)      #for maximum
+minimum_reading=min(distance)     #for minimum
 apporach=0
 backoff=0
 for i in range(1,len(distance)-1):
-    if(distance[i-1]>distance[i]):
+    if(distance[i-1]>distance[i]):  #previous>currrent =approach
         apporach+=1
-    if(distance[i]<distance[i+1]):
+    if(distance[i]<distance[i+1]): #current<next=backoff
         backoff+=1
-file.close()
-print("Total readings :",total_readings)
+file.close()     #close file 
+#output display
+print("Total readings :",total_readings)   
 print("Maximum distance:",maximum_reading)
 print("Minimum distance:",minimum_reading)
 print("No of Approach:",apporach)
 print("No of backoff:",backoff)
+
