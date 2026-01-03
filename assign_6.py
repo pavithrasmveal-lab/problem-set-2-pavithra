365f7f92dd0ec6ee4956ed29408aa208d78672cb
pavithrasmveal-lab <pavithrasmveal@gmail.com>
2026-01-03 05:55:23 +0530

    Update assign_6.py

--- assign_6.py
+++ assign_6.py
@@ -1,15 +1,20 @@
+#Smallest Subarray with Sum ≥ Target
+def smallsubarray(l,target):
+    flag=False     
+    for i in range(2,len(l)):#for sub list length
+        for j in range((len(l)-i)+1):  
+            sum=0
+            for k in range(j,j+i):   #for possible sublist for length i
+                sum=sum+int(l[k])     
+            if(sum==target):      #checking for target value
+                print(f"The smallest length:{i}")#print output
+                print(f"The subarray:{l[j:j+i]}")
+                flag=True         
+                break         #break the inner loop
+        if (flag):
+            break          #break the outer loop
+    
+    
 l=list(map(int,input("Enter inputs:").split()))  #get input
 target=int(input("Enter a number:"))   #get target value
-flag=False     
-for i in range(2,len(l)):#for sub list length
-    for j in range((len(l)-i)+1):  
-        sum=0
-        for k in range(j,j+i):   #for possible sublist for length i
-            sum=sum+int(l[k])     
-        if(sum==target):      #checking for target value
-            print(f"The smallest length:{i}")#print output
-            print(f"The subarray:{l[j:j+i]}")
-            flag=True         
-            break         #break the inner loop
-    if (flag):
-        break          #break the outer loop
+a=smallsubarray(l,target) #fuction call
